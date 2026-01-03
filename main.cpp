d911bea43f170eff8bdf611506e660df3ee3061c
arkenarusinovci <arkena.rusinovci@student.uni-pr.edu>
2026-01-03 09:37:43 -0500

    Vendosa kodin e munguar

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int numOfClasses = 0;
+	float totalSum = 0;
+	cout << "Vendos numrin e klasav që ke kryer: ";
+	cin >> numOfClasses;
+}
 
