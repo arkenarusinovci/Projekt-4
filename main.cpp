44f031d049be7e459c227c3b3caf7440dbd93284
amantrsn <amant.rusinovci@student.uni-pr.edu>
2026-01-03 10:10:34 -0500

    Vendosa per loop dhe mesataren

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 
 int main() {
-	int numOfClasses = 0;
-	float totalSum = 0;
-	cout << "Vendos numrin e klasav që ke kryer: ";
-	cin >> numOfClasses;
+    int numOfClasses = 0;
+    float totalSum = 0;
+    cout << "Vendos numrin e klasav që ke kryer: ";
+    cin >> numOfClasses;
+    for (int i = 0; i < numOfClasses; i++) {
+        int grade = 0;
+        cout << "Vendos noten qe ke marr ne lenden " << i;
+        cin >> grade;
+        totalSum += grade;
+    }
+    int avg = totalSum / numOfClasses;
+    cout << "Nota mesatare eshte: " << avg;
 }
-
